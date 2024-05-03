7c5208615121221a917f0e427bbe133d5e0d6088
Dhruvik <dhruvik.barvaliya.blackwolve@gmail.com>
2024-05-03 14:16:10 +0530

    sss

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ async def root():
 @app.post("/items")
 async def read_item(input_data: dict):
     print(input_data,"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    input_file = input_data.get("num1")
+    input_file = input_data.get("docxPath")
 
-    output_file = input_data.get("num2")
+    output_file = input_data.get("filePath")
     convert(input_file, output_file)
 
     return {"item_id": "ok"}
